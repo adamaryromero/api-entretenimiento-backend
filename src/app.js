96a529bcf5303ee89aa319acd53e69d97f976eda
Your Name <you@example.com>
2026-07-21 01:41:45 -0500

    express

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ import notisRoutes from './routes/notis.routes.js';
 const app = express();
 
 app.use(cors());
-app.use(express.json()); // para que interprete los objetos json
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Rutas
 app.use('/api', authRoutes);
